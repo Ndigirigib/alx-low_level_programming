215db0f2679476fea34b3426329295efa68abef7
Ndigirigib <ndigirigib@gmail.com>
2023-02-16 23:25:15 -0800

    4-fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: The function prints a sentence followed by new line
+ * main return: Returns nothing
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-	char sentence[100] =  "Programming is like building a multilingual puzzle";
+	char sentence[100] =  "Programming is like building a multilingual puzzle\n";
 
 	puts(sentence);
 	return (0);
