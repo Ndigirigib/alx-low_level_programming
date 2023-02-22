6c1cf6a42df81945c35f9405ab62688813234d05
Ndigirigib <ndigirigib@gmail.com>
2023-02-22 08:44:45 -0800

    quesion 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: is checked if it's a character
+ * Return: If c is a character it returns 1, otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z');
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void _putchar(char c);
 void print_alphabet(void);
 
 int _islower(int c);
-
+int _isalpha(int c);
 
 #endif
 
