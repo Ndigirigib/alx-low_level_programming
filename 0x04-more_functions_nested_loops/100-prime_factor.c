a75d6d98a31538eb271d9d60eb2c1df2be7107db
Ndigirigib <ndigirigib@gmail.com>
2023-02-24 13:11:53 -0800

    question 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * main - calculate largest prime
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int x = 612852475143;
+	long int y;
+
+	for (y = 2; y < x; y++)
+	{
+		if (x % y == 0)
+		{
+			x = x/ y;
+		}
+	}
+	printf("%ld\n", py);
+	return (0);
+}
