3cd24bf5fc0ed812d9221b3300193dfcf9155c66
Ndigirigib <ndigirigib@gmail.com>
2023-03-14 12:54:50 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *strout;
 	unsigned int i, j, k, limit;
 
-	if (sl == NULL)
+	if (s1 == NULL)
 		s1 = "";
 	if(s2 == NULL)	
-		S2 = "";
+		s2 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
