ecbcc1754c3ceec8d690a04b3dd6134d6c46bf9e
Ndigirigib <ndigirigib@gmail.com>
2023-03-15 14:11:57 -0700

    6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _strlen_recursion - return the length of a string.
+ * _strlen_recursion - returns the length of a string.
  * @s: string
  * Return: the length of a string.
  */
-
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
@@ -16,19 +15,18 @@ int _strlen_recursion(char *s)
 
 /**
  * comparator - compares each character of the string.
- * @s: string.
- * @n1: smalles iterator.
- * @n2: biggest iterator
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
  * Return: .
  */
-
 int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
 	{
 		if (n1 == n2 || n1 == n2 + 1)
 			return (1);
-		return (0 + comparator(s, n1 + 1, n2 = 1));
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
 }
@@ -38,7 +36,6 @@ int comparator(char *s, int n1, int n2)
  * @s: string.
  * Return: 1 if s is a palindrome, 0 if not.
  */
-
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
