240d16fdc088b7cfd3aeb3d8e91293383b48b3f2
Ndigirigib <ndigirigib@gmail.com>
2023-03-16 12:57:21 -0700

    ui

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,29 +11,39 @@
 
 int main(int argc, char *argv[])
 {
-	int cents, ncoins = 0;
+	int cents, coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Eror\n");
 		return (1);
 	}
 
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 	{
-		if (cents >= 25)
+		coins++;
+		if ((cents - 25) >= 0)
+		{
 			cents -= 25;
-		else if (cents >= 10)
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
 			cents -= 10;
-		else if (cents >= 5)
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
 			cents -= 5;
-		else if (cents >= 2)
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
 			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+			continue;
+		}
+		cents--;
 	}
 	printf("%d\n", ncoins);
 	return (0);
