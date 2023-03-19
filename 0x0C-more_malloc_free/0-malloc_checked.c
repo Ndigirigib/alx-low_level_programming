c69c7f6da9dd139838a1b5b4286bfb194e1324d1
Ndigirigib <ndigirigib@gmail.com>
2023-03-19 01:22:51 -0700

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,9 +9,11 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
-
-	if (mem == NULL)
+	char *p;
+	
+		
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
-	return (mem);
+	return (p);
 }
