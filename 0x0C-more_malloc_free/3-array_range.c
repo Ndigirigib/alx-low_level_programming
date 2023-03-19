28ffca217b974c9c5ca950645ad7d24a0de897d3
Ndigirigib <ndigirigib@gmail.com>
2023-03-19 04:35:30 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,12 +18,12 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(ar) * ((max - min) + 1));
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
 
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0l min <= max; i++, min++)
+	for (i = 0; min <= max; i++, min++)
 		ar[i] = min;
 
 	return (ar);
